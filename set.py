1944424c1e19e636d49927c53f88112f49ca7200
Ethan Cho <ethan.cho1826@gmail.com>
2022-06-16 17:06:15 -0500

    set practice

---
+++ set.py
@@ -0,0 +1,23 @@
+webdev = {'SQL', 'css', 'html', 'js', 'python'}
+
+data = {'R', 'SQL', 'python'}
+
+# print(type(webdev), data)
+
+# intersection of sets (anything that is in both sets)
+common_tech = webdev & data
+# another way of doing intersection
+common_tech_method = webdev.intersection(data)
+print(common_tech)
+
+# union of sets (all items from both sets, if there are duplicates only lists it once)
+everything = webdev | data
+# another way of doing union
+everything_method = webdev.union(data)
+print(everything)
+
+# difference of sets (returns new set with members from left that doesn't exist in the right set)
+no_common = webdev - data
+# another way of doing difference
+no_common_method = webdev.difference(data)
+print(no_common)
