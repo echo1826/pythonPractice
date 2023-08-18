b8ad662722aea06035fc1dd1446dfb641421c132
Ethan <ethan.cho1826@gmail.com>
2023-08-17 23:29:29 -0500

    collected the count of all squirrels that had grey, cinnamon, and black fur then output the count to another csv file

---
+++ csv-pandas/squirrel.py
@@ -0,0 +1,20 @@
+import pandas
+
+csv = pandas.read_csv("./2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_rows = csv[csv["Primary Fur Color"] == "Gray"]
+grey_fur = grey_rows["Primary Fur Color"].count()
+
+cinnamon_rows = csv[csv["Primary Fur Color"] == "Cinnamon"]
+cinnamon_fur = cinnamon_rows["Primary Fur Color"].count()
+
+black_rows = csv[csv["Primary Fur Color"] == "Black"]
+black_fur = black_rows["Primary Fur Color"].count()
+
+fur_dict = {
+    "grey": [grey_fur],
+    "cinnamon": [cinnamon_fur],
+    "black": [black_fur]
+}
+# print(fur_dict)
+fur_data_frame = pandas.DataFrame(fur_dict)
+fur_data_frame.to_csv("./squirrel_count.csv")
