ce62c8ee5839fa269aff0b59de0b23a9099b6c1b
Ethan <ethan.cho1826@gmail.com>
2023-08-18 00:37:29 -0500

    us quiz now writes the state names near the state when answered correctly

--- us-quiz/main.py
+++ us-quiz/main.py
@@ -6,10 +6,19 @@ screen.title("U.S. Quiz")
 image = "./blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-
+pen = turtle.Turtle()
+pen.penup()
+pen.hideturtle()
 data = pandas.read_csv("50_states.csv")
+num_answered = 0
 
-answer = screen.textinput(title="Guess The State", prompt="Give a state name")
+while num_answered != 50:
+    answer = screen.textinput(title=f"{num_answered}/50 States Correct", prompt="Give a state name")
+    row = data[data.state == answer.title()]
+    if len(row):
+        pen.goto(row.x.values[0], row.y.values[0])
+        pen.write(answer.title())
+        num_answered += 1
 
 
 screen.exitonclick()
