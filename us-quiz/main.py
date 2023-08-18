7119ba361dc7c7e696159eb65733175d92d567da
Ethan <ethan.cho1826@gmail.com>
2023-08-18 00:48:48 -0500

    implemented an output csv on states that were not guessed for each game

--- us-quiz/main.py
+++ us-quiz/main.py
@@ -10,15 +10,24 @@ pen = turtle.Turtle()
 pen.penup()
 pen.hideturtle()
 data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 num_answered = 0
+answer_correct = []
 
 while num_answered != 50:
     answer = screen.textinput(title=f"{num_answered}/50 States Correct", prompt="Give a state name")
     row = data[data.state == answer.title()]
+    if answer.title() == "Exit":
+        break
     if len(row):
         pen.goto(row.x.values[0], row.y.values[0])
         pen.write(answer.title())
         num_answered += 1
+        answer_correct.append(answer.title())
 
+for state in answer_correct:
+    all_states.remove(state)
+
+pandas.DataFrame(all_states).to_csv("missing_states.csv")
 
 screen.exitonclick()
