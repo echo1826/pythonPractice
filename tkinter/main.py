c9cc222a6b34252b3034a63b58e96eab631e754a
Ethan <ethan.cho1826@gmail.com>
2023-08-24 13:32:46 -0500

    event listeners, inputs, buttons

--- tkinter/main.py
+++ tkinter/main.py
@@ -9,6 +9,21 @@ window.minsize(width=500, height=300)
 label = tkinter.Label(text="I am a Label", font=("Arial", 24))
 # .pack() will render the component to the screen
 label.pack()
+label["text"] = "New Text"
+label.config(text="More New Text")
+
+
+
+def click_handler():
+    label.config(text=input.get())
+# command is an event listener for on click
+button = tkinter.Button(text="Click Me", command=click_handler)
+button.pack()
+
+# input
+input = tkinter.Entry(width=10)
+input.pack()
+
 
 # keeps window on screen
 window.mainloop()
