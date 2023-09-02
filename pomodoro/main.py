168290bc6cd26f845b5741c09d9be8ec5f849290
Ethan <ethan.cho1826@gmail.com>
2023-09-01 19:11:00 -0500

    added buttons to UI

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -19,11 +19,21 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", font=("Arial", 24, "bold"), bg=YELLOW, fg=GREEN)
+timer_label.pack()
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=tomato_image)
 canvas.create_text(103, 130, text="00:00", fill="white", font=("Arial", 30, "bold"))
 canvas.pack()
+
+
+start_btn = Button(text="Start")
+start_btn.pack(side="right")
+
+reset_btn = Button(text="Reset")
+reset_btn.pack(side="left")
 
 window.mainloop()
