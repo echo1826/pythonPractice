815814324656d634c432789011c3c1d9d7b795ee
Ethan <ethan.cho1826@gmail.com>
2024-01-31 12:25:35 -0600

    starter code and changed hard coded questions to fetch from api

---
+++ quiz_app/data.py
@@ -0,0 +1,15 @@
+import requests
+
+api_url = "https://opentdb.com/api.php"
+params = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(api_url, params=params)
+response.raise_for_status()
+data = response.json()
+print(data)
+
+question_data = data["results"]
+print(question_data)
