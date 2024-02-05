da17bec535839c1a233611a0cfb8639b2ff6366f
Ethan <ethan.cho1826@gmail.com>
2024-02-05 14:49:33 -0600

    post request to create a graph for account

--- pixela/main.py
+++ pixela/main.py
@@ -1,13 +1,30 @@
 import requests
 
+USERNAME = "echo1108"
+TOKEN = "secretshhhhhhh"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "secretshhhhhhh",
-    "username": "echo1108",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-response = requests.post(url=pixela_endpoint, json=user_params) # json is put into the req.body of the request
-response.raise_for_status()
+# response = requests.post(url=pixela_endpoint, json=user_params) # json is put into the req.body of the request
+# print(response.status_code)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "days",
+    "type": "int",
+    "color": "sora"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers) # how to send request headers in a fetch
 print(response.text)
